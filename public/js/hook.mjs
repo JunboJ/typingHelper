3c16defeb2b21d48b6129c2e29037343a2a5803e
JunboJ <junboz598@gmail.com>
2019-12-07 22:29:51 +1300

    bugs fixed, hosting font awesome

--- public/js/hook.mjs
+++ public/js/hook.mjs
@@ -1,40 +1,50 @@
 import { getCursorXY } from './typeHelper.mjs';
 
-const elements = document.getElementsByClassName('writingHelper');
-let elementsArr = Array.from(elements);
-
-elementsArr.forEach(element => {
-    console.log(element);
-    element.parentElement.style.display = 'flex';
-    element.parentElement.style.flexFlow = 'column';
-    element.parentElement.style.width = 'inherit';
-
-    const getHelperDiv = () => {
-        return document.getElementsByClassName('helperDiv')[0] ? document.getElementsByClassName('helperDiv')[0] : null;
+$(document).ready(function () {
+    // const elements = $('.writingHelper');
+    // document.getElementsByClassName('writingHelper');
+    // let elementsArr = Array.from(elements);
+    const writingHelper = (element) => {
+        getCursorXY(element);
     }
-    const eventHandler = event => {
-        writingHelper(element);
-    }
-    const arrowKeyEventHandler = event => {
-        // console.log(event.which);
-        if (
-            event.which == 37 ||
-            event.which == 38 ||
-            event.which == 39 ||
-            event.which == 40
-        ) {
+
+    $('.writingHelper').each(function (index, object) {
+        // elementsArr.forEach(element => {
+        const element = $(this);
+        console.log(element[0]);
+        
+        element.parent().css({ 'display': 'flex', 'flex-flow': 'column', 'width': 'inherit' });
+        // element.parentElement.style.display = 'flex';
+        // element.parentElement.style.flexFlow = 'column';
+        // element.parentElement.style.width = 'inherit';
+        
+        const getHelperDiv = () => {
+            return $('.helperDiv')[0] || null;
+        }
+
+        const eventHandler = event => {
             writingHelper(element);
         }
-        if (event.which == 27) {
-            const div = getHelperDiv();
-            if (div) document.body.removeChild(div);
-        }
-    }
-    element.addEventListener('mouseup', eventHandler);
-    element.addEventListener('input', eventHandler);
-    element.addEventListener('keyup', arrowKeyEventHandler);
-});
 
-const writingHelper = (element) => {
-    getCursorXY(element);
-}
+        const arrowKeyEventHandler = event => {
+            // console.log(event.which);
+            if (
+                event.which == 37 ||
+                event.which == 38 ||
+                event.which == 39 ||
+                event.which == 40
+            ) {
+                writingHelper(element);
+            }
+            if (event.which == 27) {
+                console.log('key 27');
+                const div = getHelperDiv();
+                if (div) div.remove();
+            }
+        }
+
+        element.on('click', eventHandler);
+        element.on('input', eventHandler);
+        element.on('keyup', arrowKeyEventHandler);
+    });
+});
--- public/js/typeHelper.mjs
+++ public/js/typeHelper.mjs
@@ -1,9 +1,11 @@
 import { getHelperLibrary } from './helperLibrary.mjs';
 
 export const getCursorXY = (input) => {
+  const inputHtml = input[0];
+  
   const caretPosition = {
-    cursorStart: input.selectionStart,
-    cursorEnd: input.selectionEnd
+    cursorStart: inputHtml.selectionStart,
+    cursorEnd: inputHtml.selectionEnd
   }
   // initiate the selectionpoint
   let cursorStart = caretPosition.cursorStart;
@@ -13,21 +15,59 @@ export const getCursorXY = (input) => {
   // get the character under caret
   if (cursorStart == cursorEnd) cursorStart = cursorEnd == 0 ? 0 : (cursorStart - 1);
   // console.log('cursorStart:' + cursorStart + ' cursorEnd:' + cursorEnd);
-  const inputString = input.value;
+  const inputString = inputHtml.value;
   const currentCharacter = inputString.slice(cursorStart, cursorEnd);
+  // console.log(currentCharacter);
 
   // get options from library
   const options = currentCharacter.length == 1 ? getHelperLibrary(currentCharacter) : null;
-
+  // console.log(options);
 
   let helperDiv = document.getElementsByClassName('helperDiv')[0] ? document.getElementsByClassName('helperDiv')[0] : null;
 
-  // helper content creating function
+  // closeBtn event handler
+  const closeBtnClickedHandler = helperDiv => {
+    helperDiv.remove();
+    input.off('keyup', keyupEventHandler);
+    input.off('keydown', keydownEventHandler);
+  };
 
+  // onblur event handler
+  const onBlurHandler = helperDiv => {
+    closeBtnClickedHandler(helperDiv);
+    console.log('blur');
+  };
+
+  // add key press event handler
+  const keyupEventHandler = event => {
+    if (document.getElementById(event.which)) {
+      event.preventDefault();
+      // document.getElementById(event.which).onmouseup();
+      console.log($(event.which));
+      console.log(`#${event.which}`);
+      $(`#${event.which}`).mouseup();
+    }
+  };
+
+  const keydownEventHandler = event => {
+    if (document.getElementById(event.which)) {
+      event.preventDefault();
+      document.getElementById(event.which).style.backgroundColor = 'rgb(78, 161, 216)';
+    }
+  };
+
+  // remove helperDiv function
+  const removeHelper = (helperDiv) => {
+    helperDiv.remove();
+    input.off('keyup', keyupEventHandler);
+    input.off('keydown', keydownEventHandler);
+  };
+
+  // helper content creating function
   const createOptions = helperDiv => {
     const prevHelperContent = document.getElementsByClassName('helperContent')[0] ? document.getElementsByClassName('helperContent')[0] : null;
     if (prevHelperContent !== null) {
-      helperDiv.removeChild(prevHelperContent);
+      prevHelperContent.remove();
     }
     const helperContent = document.createElement('div');
     helperContent.className = 'helperContent';
@@ -51,78 +91,55 @@ export const getCursorXY = (input) => {
       let tip = document.createElement('small');
       tip.className = 'tips_windows';
       tip.innerHTML = (index + 1);
-      
+
       helperOptions.prepend(tip);
       // add click event listener
-      const clickEventHandler = event => {
-        input.focus();
-        let newString = inputString.slice(0, cursorStart) + char + inputString.slice(cursorEnd);
-        input.value = newString;
-        document.body.removeChild(helperDiv);
-        helperDiv = null;
-        document.body.removeEventListener('keyup', keyupEventHandler);
-        document.body.removeEventListener('keydown', keydownEventHandler);
+      const mouseDownHandler = event => {
+        event.stopPropagation();
+        input.off('blur');
       };
-      helperOptions.addEventListener('click', clickEventHandler);
+      
+      const mouseUpHandler = event => {
+        let newString = inputString.slice(0, cursorStart) + char + inputString.slice(cursorEnd);
+        inputHtml.value = newString;
+        removeHelper(helperDiv);
+        inputHtml.focus();
+      }
+      $(helperOptions).on('mousedown', mouseDownHandler);
+      $(helperOptions).on('mouseup', mouseUpHandler);
     });
 
     // closeBtn event listener
-    const closeBtnClickedHandler = event => {
-      document.body.removeChild(helperDiv);
-    };
-
-    const escBtnPressedHandler = event => {
-      console.log(event.which);
-    };
-
     const closeBtn = document.createElement('button');
     closeBtn.className = 'helperCloseBtn';
-    closeBtn.innerHTML = '&times;';
+    closeBtn.innerHTML = '<i class="fas fa-times"></i>';
     helperContent.appendChild(closeBtn);
-    closeBtn.addEventListener('click', closeBtnClickedHandler);
-    // input.addEventListener('keypress', escBtnPressedHandler);
+    closeBtn.addEventListener('click', () => closeBtnClickedHandler(helperDiv));
     helperDiv.appendChild(helperContent);
+    // console.log(helperDiv);
 
-    // add key press event listener to document
-    const keyupEventHandler = event => {
-      // console.log(event.cancelable);
-      if (document.getElementById(event.which)) {
-        event.preventDefault();
-        document.getElementById(event.which).click();
-      }
-    };
-
-    const keydownEventHandler = event => {
-      if (document.getElementById(event.which)) {
-        event.preventDefault();
-        document.getElementById(event.which).style.backgroundColor = 'rgb(78, 161, 216)';
-      }
-    }
-
-    document.body.addEventListener('keyup', keyupEventHandler);
-    document.body.addEventListener('keydown', keydownEventHandler);
   };
 
   // display writing helper
 
   if (options !== null) {
     // create the clone and configure the style
-    const parent = input.parentElement;
+    const parent = inputHtml.parentElement;
     const cloneField = document.createElement('div');
-    const copyStyle = getComputedStyle(input);
+    const copyStyle = getComputedStyle(inputHtml);
     for (const prop of copyStyle) { cloneField.style[prop] = copyStyle[prop]; }
-    if (input.tagName === 'TEXTAREA') cloneField.style.height = 'auto';
-    if (input.tagName === 'INPUT') cloneField.style.width = 'auto';
+    if (inputHtml.tagName === 'TEXTAREA') cloneField.style.height = 'auto';
+    if (inputHtml.tagName === 'INPUT') cloneField.style.width = 'auto';
     // create span for locating caret on screen
     const span = document.createElement('span');
 
     // replace spaces in the textcontent
     const swap = '.';
-    const inputValue = input.tagName === 'INPUT' ? input.value.replace(/ /g, swap) : input.value;
-    const textContent = inputValue.substr(0, selectionPoint);
+    const inputValue = inputHtml.tagName === 'INPUT' ? inputHtml.value.replace(/ /g, swap) : inputHtml.value;
+    const textContent = inputValue.substr(0, selectionPoint).slice(0, -1);
 
     // assign content
-    span.textContent = inputValue.substr(selectionPoint) || '.';
+    span.textContent = inputValue.substr(selectionPoint) + '.' || '.';
     cloneField.textContent = textContent;
 
     cloneField.appendChild(span);
@@ -150,9 +167,10 @@ export const getCursorXY = (input) => {
       offsetTop: inputY,
       offsetWidth: inputWidth,
       offsetHeight: inputHeight
-    } = input;
+    } = inputHtml;
 
-    parent.removeChild(cloneField);
+    // parent.removeChild(cloneField);
+    cloneField.remove();
 
     // create helper box
     if (helperDiv === null) {
@@ -160,7 +178,6 @@ export const getCursorXY = (input) => {
       helperDiv = document.createElement('div');
       helperDiv.className = 'helperDiv';
       document.body.appendChild(helperDiv);
-
     }
     createOptions(helperDiv);
 
@@ -169,17 +186,40 @@ export const getCursorXY = (input) => {
       offsetHeight: helperHeight
     } = helperDiv;
 
-    const leftPosition = (inputX + spanX - 5);
+    const windowWidth = window.innerWidth;
+    // console.log('windowWidth: ' + windowWidth);
+    const windowHeight = window.innerHeight;
+    let leftPosition = (inputX + spanX + 3);
+    let spanW = inputX + spanX + helperWidth;
+    // console.log('spanW: ' + spanW);
     const topPosition = (inputY - helperHeight + cloneFieldHeight - spanHeight - 10);
+
+    if (inputHtml.tagName === 'INPUT') {
+      if ((spanW - helperWidth) >= inputWidth && (inputWidth + helperWidth) < windowWidth) {
+        leftPosition = inputX + inputWidth;
+      }
+      if (spanW >= windowWidth && (inputWidth + helperWidth) > windowWidth) {
+        leftPosition = windowWidth - helperWidth + 3;
+      }
+    }
+
+    if (inputHtml.tagName === 'TEXTAREA') {
+      if (spanW >= windowWidth) {
+        leftPosition = windowWidth - helperWidth + 3;
+      }
+    }
 
     helperDiv.style.left = `${leftPosition}px`;
     helperDiv.style.top = `${topPosition}px`;
     // console.log('inputX: ' + inputX + '  spanX: ' + spanX + '  cloneFieldHeight: ' + cloneFieldHeight);
 
+    // add key press event listener
+    input.on('keyup', keyupEventHandler);
+    input.on('keydown', keydownEventHandler);
+    // input.one("blur", () => onBlurHandler(helperDiv));
   } else {
     if (helperDiv !== null) {
-      document.body.removeChild(helperDiv);
-      helperDiv = null;
+      removeHelper(helperDiv);
     }
   };
 };
