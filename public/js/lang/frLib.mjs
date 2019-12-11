4fc33d820daeba7f2c870e0b2bb6ea660ae82fd4
JunboJ <junboz598@gmail.com>
2019-12-12 11:41:53 +1300

    changes

--- public/js/lang/frLib.mjs
+++ public/js/lang/frLib.mjs
@@ -19,48 +19,33 @@ export const get_fr = character => {
     switch (character) {
         case 'a':
             return fr.a;
-            break;
         case 'A':
             return fr.A;
-            break;
         case 'c':
             return fr.c;
-            break;
         case 'C':
             return fr.C;
-            break;
         case 'e':
             return fr.e;
-            break;
         case 'E':
             return fr.E;
-            break;
         case 'i':
             return fr.i;
-            break;
         case 'I':
             return fr.I;
-            break;
         case 'o':
             return fr.o;
-            break;
         case 'O':
             return fr.O;
-            break;
         case 'u':
             return fr.u;
-            break;
         case 'U':
             return fr.U;
-            break;
         case 'y':
             return fr.y;
-            break;
         case 'Y':
             return fr.Y;
-            break;
         default:
             return null;
-            break;
     }
 };
