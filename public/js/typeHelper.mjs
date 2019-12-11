74a8af4db1b81bea0de3f0c828485f23807d3cc3
JunboJ <junboz598@gmail.com>
2019-12-11 19:28:59 +1300

    front-end updates, menu container added, settings button added

--- public/js/typeHelper.mjs
+++ public/js/typeHelper.mjs
@@ -3,7 +3,7 @@ import { get_fr } from './lang/frLib.mjs';
 export const getCursorXY = (input) => {
   const inputHtml = input[0];
   const inputParent = input.parent();
-  
+
   const caretPosition = {
     cursorStart: inputHtml.selectionStart,
     cursorEnd: inputHtml.selectionEnd
@@ -24,6 +24,10 @@ export const getCursorXY = (input) => {
   let helperDiv = $('.helperDiv')[0] ? $('.helperDiv')[0] : null;
 
   // closeBtn event handler
+  const closeBtnMouseDownHandler = event => {
+    event.stopPropagation();
+    input.off('blur');
+  }
   const closeBtnClickedHandler = helperDiv => {
     helperDiv.remove();
     input.off('keyup', keyupEventHandler);
@@ -53,6 +57,31 @@ export const getCursorXY = (input) => {
     }
   };
 
+  // cog onclick handler
+  const settingBtnClickedHandler = event => {
+    event.stopPropagation();
+    input.off('blur');
+    let display = $('.settingMenuWrapper').css('display');
+    const position = $('.helperDiv')[0].classList.contains('helperDiv-bottom');
+    console.log(position);
+    if (display == 'none') {
+      const menuHeight = $('.settingMenuWrapper').outerHeight();
+      let helperY = $('.helperDiv').css('top').slice(0, -2);
+      helperY = parseInt(helperY);
+      $('.settingMenuWrapper').css({ 'display': 'flex' });
+      const currentY = position ? helperY : helperY - menuHeight;
+      $('.helperDiv').css({'top': currentY});
+    }
+    if (display == 'flex') {
+      const menuHeight = $('.settingMenuWrapper').outerHeight();
+      let helperY = $('.helperDiv').css('top').slice(0, -2);
+      helperY = parseInt(helperY);
+      const currentY = position ? helperY : helperY + menuHeight;
+      $('.helperDiv').css({'top': currentY});
+      $('.settingMenuWrapper').css({ 'display': 'none' });
+    };
+  }
+
   // remove helperDiv function
   const removeHelper = (helperDiv) => {
     helperDiv.remove();
@@ -60,35 +89,44 @@ export const getCursorXY = (input) => {
     input.off('keydown', keydownEventHandler);
   };
 
+  // setting drop down list active function
+  const createDropdown = () => {
+    const menu = document.createElement('div');
+    const mc = document.createElement('span');
+    const mctext = document.createTextNode('More coming');
+    mc.appendChild(mctext);
+    menu.appendChild(mc);
+    menu.className = 'dropdown-menu';
+    return menu;
+  }
+
   // helper content creating function
-  const createOptions = helperDiv => {
-    const prevHelperContent = document.getElementsByClassName('helperContent')[0] ? document.getElementsByClassName('helperContent')[0] : null;
-    if (prevHelperContent !== null) {
-      prevHelperContent.remove();
-    }
-    const helperContent = document.createElement('div');
-    helperContent.className = 'helperContent';
+  const createOptions = (helperDiv, helperContent) => {
+    $(helperContent).empty();
 
     let className = 'helperOptions';
-
     // map options to buttons
     options.map((char, index) => {
       let key = (index + 49);
+
       // create options as buttons
       let helperOptions = document.createElement('button');
       helperOptions.className = className;
       helperOptions.id = key;
       helperOptions.style.fontSize = '1rem';
+
       // helperOptions.style.fontWeight = 'bold';
       let text = document.createTextNode(char);
       helperOptions.appendChild(text);
       helperContent.appendChild(helperOptions);
+
       // create tip spans
       let tip = document.createElement('small');
       tip.className = 'tips_windows';
       tip.innerHTML = (index + 1);
 
       helperOptions.prepend(tip);
+
       // add click event listener
       const mouseDownHandler = event => {
         event.stopPropagation();
@@ -101,18 +139,11 @@ export const getCursorXY = (input) => {
         removeHelper(helperDiv);
         inputHtml.focus();
       }
+
       $(helperOptions).on('mousedown', mouseDownHandler);
       $(helperOptions).on('mouseup', mouseUpHandler);
+
     });
-
-    // closeBtn event listener
-    const closeBtn = document.createElement('button');
-    closeBtn.className = 'helperCloseBtn';
-    closeBtn.innerHTML = '<i class="fas fa-times"></i>';
-    helperContent.appendChild(closeBtn);
-    closeBtn.addEventListener('click', () => closeBtnClickedHandler(helperDiv));
-    helperDiv.appendChild(helperContent);
-
   };
 
   // display writing helper
@@ -174,13 +205,47 @@ export const getCursorXY = (input) => {
     cloneField.remove();
 
     // create helper box
+    let helperContent = $('.helperContent')[0] || null;
     if (helperDiv === null) {
-      console.log('new helper!');
       helperDiv = document.createElement('div');
       helperDiv.className = 'helperDiv';
       inputParent.append(helperDiv);
+
+      // option container
+      helperContent = document.createElement('div');
+      helperContent.className = 'helperContent';
+      helperDiv.append(helperContent);
+
+      // button set container
+      const btnSet = document.createElement('div');
+      btnSet.className = 'btnSet';
+      helperDiv.append(btnSet);
+
+      // setting menu container
+      const settingMenuWrapper = document.createElement('div');
+      settingMenuWrapper.className = 'settingMenuWrapper';
+      const menuContent = createDropdown();
+      settingMenuWrapper.appendChild(menuContent);
+      helperDiv.append(settingMenuWrapper);
+
+      // setting button
+      const settingBtn = document.createElement('button');
+      settingBtn.className = 'setttingBtn';
+      settingBtn.innerHTML = '<i class="fas fa-cog"></i>';
+      btnSet.appendChild(settingBtn);
+      $(settingBtn).on('mousedown', settingBtnClickedHandler);
+
+
+      // closeBtn 
+      const closeBtn = document.createElement('button');
+      closeBtn.className = 'helperCloseBtn';
+      closeBtn.innerHTML = '<i class="fas fa-times"></i>';
+      btnSet.appendChild(closeBtn);
+      closeBtn.addEventListener('mousedown', closeBtnMouseDownHandler);
+      closeBtn.addEventListener('mouseup', () => closeBtnClickedHandler(helperDiv));
+
     }
-    createOptions(helperDiv);
+    createOptions(helperDiv, helperContent);
 
     const {
       offsetWidth: helperWidth,
@@ -190,32 +255,33 @@ export const getCursorXY = (input) => {
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
 
+    // styling
     let leftPosition = ipX + spanX - 16;
     let spanW = ipX + spanX + helperWidth;
-    let topPosition = (0 + cloneFieldHeight - helperHeight - spanHeight - 10);
-    let topToWindow = cloneFieldHeight - ipY;
-console.log(cloneFieldHeight);
-console.log(spanHeight);
-
+    let topPosition = (cloneFieldHeight - helperHeight - spanHeight - 10);
+    let topToWindow = Math.abs(spanY - inputHeight - ipY);
+    if (span.textContent !== '.') {
+      topToWindow = Math.abs(spanHeight - ipY);
+    }
     if (topToWindow <= helperHeight) {
       topPosition = (cloneFieldHeight);
-      $('.helperDiv').removeClass('helperDiv-bottom');
-      $('.helperOptions').removeClass('helperOptions-bottom');
-      $('.helperDiv').addClass('helperDiv-top');
-      $('.helperOptions').addClass('helperOptions-top');
-    } else {
       $('.helperDiv').addClass('helperDiv-bottom');
       $('.helperOptions').addClass('helperOptions-bottom');
       $('.helperDiv').removeClass('helperDiv-top');
       $('.helperOptions').removeClass('helperOptions-top');
+    } else {
+      $('.helperDiv').removeClass('helperDiv-bottom');
+      $('.helperOptions').removeClass('helperOptions-bottom');
+      $('.helperDiv').addClass('helperDiv-top');
+      $('.helperOptions').addClass('helperOptions-top');
     }
 
     if (inputHtml.tagName === 'INPUT') {
       if ((spanW - helperWidth) >= inputWidth && (inputWidth + helperWidth) < windowWidth) {
-        leftPosition = inputX + inputWidth;
+        leftPosition = ipX + inputWidth;
       }
       if (spanW >= windowWidth && (inputWidth + helperWidth) > windowWidth) {
-        leftPosition = windowWidth - helperWidth + 3;
+        leftPosition = windowWidth - helperWidth - ipX;
       }
     }
 
@@ -231,7 +297,7 @@ console.log(spanHeight);
     // add key press event listener
     input.on('keyup', keyupEventHandler);
     input.on('keydown', keydownEventHandler);
-    input.one("blur", () => onBlurHandler(helperDiv));
+    // input.one("blur", () => onBlurHandler(helperDiv));
   } else {
     if (helperDiv !== null) {
       removeHelper(helperDiv);
